6256040e8ee88dfa6c34017e4e7728882c519dfc
vagrant <vagrant@rails-dev-box>
2014-10-12 19:02:00 +0000

    Upadated app layout and welcome views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Bloccit::Application.routes.draw do
-  #get "welcome/index"
-  #get "welcome/about"
+  get "welcome/index"
+  get "welcome/about"
+
+    root to: 'welcome#index'
+    
+    
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
