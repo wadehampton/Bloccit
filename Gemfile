1281aab308d1d17d6f95cb38b9c7d684bb537121
vagrant <vagrant@rails-dev-box>
2014-10-16 01:24:31 +0000

    Added seed script.

--- Gemfile
+++ Gemfile
@@ -36,6 +36,8 @@ gem 'jbuilder', '~> 1.2'
 
 gem 'bootstrap-sass', '~> 3.1.1'
 
+gem 'faker'
+
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
   gem 'sdoc', require: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+require 'faker'
+
+
+50.times do
+  Post.create!(
+     title: Faker::Lorem.sentence,
+     body:  Faker::Lorem.paragraph
+  )
+  end
+  posts = Post.all
+
+
+  100.times do
+    Comment.create!(
+    post: posts.sample,
+    body: Faker::Lorem.paragraph
+  )
+  end
+
+  puts "Seed finished"
+  puts "#{Post.count} post created"
+  puts "#{Comment.count} comments created"
+
+  # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
