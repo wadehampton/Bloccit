ab080da3093a488ea2f88251fa238abb5700e578
vagrant <vagrant@rails-dev-box>
2014-11-02 18:36:31 +0000

    Fixed production default URL & refactored

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,7 @@ class PostsController < ApplicationController
 
   def create
     @topic = Topic.find(params[:topic_id])
-    @post = current_user.posts.build(params.require(:post).permit(:title, :body))
+    @post = current_user.posts.build(post_params)
     @post.topic = @topic
     authorize @post
 
@@ -37,7 +37,7 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     authorize @post 
 
-    if @post.update_attributes(params.require(:post).permit(:title, :body))
+    if @post.update_attributes(post_params)
       flash[:notice] = "Post was updated."
       redirect_to [@topic, @post]
     else
@@ -46,3 +46,10 @@ class PostsController < ApplicationController
     end
   end
 end
+
+private
+
+def post_params
+  params.require(:post).permit(:title, :body)
+end
+
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,5 +79,5 @@ Bloccit::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # http://bloccit.herokuapp.com
-  config.action_mailer.default_url_options = { host: 'bloccit.herokuapp.com'}
+  config.action_mailer.default_url_options = { host: 'tjlandis-bloccit.herokuapp.com'}
 end
